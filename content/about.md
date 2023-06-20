8c23b236be0a55af267f019723fbd6d13171b9e0
vladmikheyev <93609096+vladmikheyev@users.noreply.github.com>
2023-06-20 13:39:09 +0300

    работа над галереей fancybox

--- content/about.md
+++ content/about.md
@@ -5,9 +5,17 @@ date = "2023-01-12"
 author = "Владислав Михеев"
 +++
 
-<!--![](/img/my-photo.jpg)-->
 
-{{<card gallery="gallery" src="/img/SportIN.jpg" src1="/img/my-photo-1.jpg" src2="/img/SportIN-2.jpg" src3="" src4="/img/GAU.jpg" src5="">}}
+
+{{<card gallery="gallery" src0="/img/my-photo-1.jpg"
+src1="/img/1-GAU.jpg"
+src2="/img/2-SportIN.jpg"  
+src3="/img/3-SportIN-2.jpg"
+src4="/img/4-IMG_2019-03-24.jpg"
+src5="/img/5-IMG_2018-06-18.jpg"
+src6="/img/6-332A3124m.jpg"
+src7="/img/7-IMG_266.jpg"
+src8="/img/8-IMG_267.jpg" >}}
 
 #
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,57 @@
+import { Carousel } from '@fancyapps/ui/dist/carousel/carousel.esm.js';
+import '@fancyapps/ui/dist/carousel/carousel.css';
+
+import { Thumbs } from '@fancyapps/ui/dist/carousel/carousel.thumbs.esm.js';
+import '@fancyapps/ui/dist/carousel/carousel.thumbs.css';
+
 import { Fancybox } from '@fancyapps/ui/dist/fancybox/fancybox.esm.js';
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
 import './style.css';
 
+import './style.css';
+
+new Carousel(
+  document.getElementById('productCarousel'),
+  {
+    infinite: false,
+    Dots: false,
+    Thumbs: {
+      type: 'classic',
+      Carousel: {
+        slidesPerPage: 1,
+        Navigation: true,
+        center: true,
+        fill: true,
+        dragFree: true,
+      },
+    },
+  },
+  { Thumbs }
+);
+
 Fancybox.bind('[data-fancybox="gallery"]', {
-  compact: false,
   idle: false,
-
-  animated: false,
-  showClass: false,
-  hideClass: false,
-
+  compact: false,
   dragToClose: false,
 
+  animated: false,
+  showClass: 'f-fadeSlowIn',
+  hideClass: false,
+
+  Carousel: {
+    infinite: false,
+  },
+
   Images: {
-    // Disable animation from/to thumbnail on start/close
     zoom: false,
+    Panzoom: {
+      maxScale: 1.5,
+    },
   },
 
   Toolbar: {
+    absolute: true,
     display: {
       left: [],
       middle: [],
@@ -26,10 +59,22 @@ Fancybox.bind('[data-fancybox="gallery"]', {
     },
   },
 
-  Thumbs: false,
+  Thumbs: {
+    type: 'classic',
+    Carousel: {
+      axis: 'x',
 
-  Carousel: {
-    // Remove the navigation arrows
-    Navigation: true,
+      slidesPerPage: 1,
+      Navigation: true,
+      center: true,
+      fill: true,
+      dragFree: true,
+
+      breakpoints: {
+        '(min-width: 640px)': {
+          axis: 'y',
+        },
+      },
+    },
   },
 });
